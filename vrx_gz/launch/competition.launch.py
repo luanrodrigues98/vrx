fcc0df368da8cfd09e6d4fec6ceaf74327be7075
Luan Rodrigues <luan20131@gmail.com>
2025-06-16 06:17:15 -0300

    channing spwan mode

--- vrx_gz/launch/competition.launch.py
+++ vrx_gz/launch/competition.launch.py
@@ -70,9 +70,14 @@ def launch(context, *args, **kwargs):
                     m.set_urdf(robot_urdf)
                 models.append(m)
         elif spawn_mode == 'random':
+            low = -400
+            high = 400
+            vals = random.sample(range(low, high+1), swarm_size * 2)
+            driff_places = [ vals[i*2:(i+1)*2] for i in range(swarm_size) ]
+
             for i in range(swarm_size):
-                x = -182 + random.uniform(-150, 150)
-                y = -663 + random.uniform(-150, 150)
+                x = -182 + driff_places[i][0]
+                y = -663 + driff_places[i][1]
                 _id = i + 1 
                 if swarm_size > 1:
                     vechicle_id = f'wamv{_id}_{iteration}_{particle_id}'
